fdd0061c5d61d30b32b439f230db2a2cfe68cfca
unknown <ahmedhazm41@gmail.com>
2025-01-25 20:04:39 +0300

    21

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ErrorBoundary from "./components/ErrorBoundary";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
